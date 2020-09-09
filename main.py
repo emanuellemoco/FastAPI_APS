0ee6b6f1a510623a34aff784d636f564fa0c8971
Emanuelle <emanuellesmoco@gmail.com>
2020-09-08 21:50:59 -0300

    Request criadas, delete e create funcionando perfeitamente. > > Co-authored-by: zMendes <leonardom1808@gmail.com>

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ from typing import Optional, List
 from fastapi import FastAPI
 from pydantic import BaseModel
 class Task(BaseModel):
+    id: int
     name: str
     description: Optional[str] = None
     status: bool = False
@@ -21,35 +22,56 @@ app = FastAPI()
 
 tasks : List[Task] = [
     {
+        "id":0,
         "name":"nome",
         "description":"abc",
         "status":False
+    },
+    {
+        "id":1,
+        "name":"nome1",
+        "description":"qqq",
+        "status":False
+    },
+    {
+        "id":2,
+        "name":"nome2",
+        "description":"ff",
+        "status":True
     }
 ]
 
+id = 2
 
 
 #ok
 @app.post("/create/")
 async def create_task(task: Task):
+    global id
+    id+=1
+    task.id = id
     tasks.append(task)
     return task
 
-#+/- ok
-#arrumar os argumentos que recebe
+#ok
 @app.put("/edit/")
-async def edit_task(task: Task):
+async def edit_task(id: int, description: str):
     
     for i in range(len(tasks)):
-        if task.name == tasks[i]["name"]:
-            tasks[i]["description"] = task.description
-            return task
+        if id == tasks[i]["id"]:
+            tasks[i]["description"] = description
+            return "Success"
 
     return "Error, name does not match with any." 
 
+#ok
 @app.put("/check/")
-async def check_task(task: Task):
-    return task
+async def check_task(status: bool,id: int):
+    for i in range(len(tasks)):
+        if id == tasks[i]["id"]:
+            tasks[i]["status"] = status
+
+    return status
 
 #ok
 @app.delete("/delete/")
@@ -57,7 +79,25 @@ async def delete_task(task: Task):
     tasks.remove(task)
     return task
 
-#falta as queries
+#listagem de tarefas concluidas, não-concluidas, ou todas
 @app.get("/list/")
-async def list_task():
+async def list_task(q: Optional[bool] = None):
+    #if q is not None:
+        #if q:
+            #filtered_tasks = filtering(tasks,True)
+            #filtered_tasks =  [x for x in tasks if x["status"] == True]
+        #return n concluídas
+        #else:
+            #filtered_tasks = filtering(tasks,False)
+            #filtered_tasks =  [x for x in tasks if not x["status"] == False]
+        
+        #return filtered_tasks
+    
+    
+    return tasks
+
+    
+            
+
+        
     return tasks
