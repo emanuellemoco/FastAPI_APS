72c448a09caa6c8e1f9d97b494ab6a88b1a55c0d
Leonardo <leonardom1808@gmail.com>
2020-09-10 16:32:27 -0300

    Mudou estrutura do tasks
    Co-authored-by: emanuellemoco emanuellesmoco@gmail.com

--- main.py
+++ main.py
@@ -8,96 +8,76 @@
 #     Deve-se permitir a listagem de tarefas concluidas, não-concluidas, ou todas
 
 from typing import Optional, List
+import uuid
+from uuid import UUID
 
 from fastapi import FastAPI
 from pydantic import BaseModel
 class Task(BaseModel):
-    id: int
-    name: str
+    name: Optional[str] = None
     description: Optional[str] = None
-    status: bool = False
+    status:  Optional[bool] = False
 
 
 app = FastAPI()
 
-tasks : List[Task] = [
-    {
-        "id":0,
+tasks = {
+    uuid.uuid4(): {
         "name":"nome",
         "description":"abc",
-        "status":False
-    },
-    {
-        "id":1,
+        "status":False },
+    uuid.uuid4(): {
         "name":"nome1",
         "description":"qqq",
-        "status":False
-    },
-    {
-        "id":2,
+        "status":False },
+    uuid.uuid4(): {
         "name":"nome2",
         "description":"ff",
-        "status":True
-    }
-]
-
-id = 2
+        "status":True}
+}
 
 
 #ok
 @app.post("/create/")
 async def create_task(task: Task):
-    global id
-    id+=1
-    task.id = id
-    tasks.append(task)
-    return task
-
-#ok
-@app.put("/edit/")
-async def edit_task(id: int, description: str):
     
-    for i in range(len(tasks)):
-        if id == tasks[i]["id"]:
-            tasks[i]["description"] = description
-            return "Success"
-
-    return "Error, name does not match with any." 
+    task.id = uuid.uuid4()
+    #tasks.append(task)
+    tasks[uuid] = task
+    return task.id
 
 #ok
-@app.put("/check/")
-async def check_task(status: bool,id: int):
-    for i in range(len(tasks)):
-        if id == tasks[i]["id"]:
-            tasks[i]["status"] = status
+@app.patch("/edit/{id}")
+async def edit_tas(id: uuid.UUID, task: Task):
+    if task.description!= None:
+        tasks[id]["description"] = task.description 
+            
+    return tasks[id]
 
-    return status
+
 
 #ok
-@app.delete("/delete/")
-async def delete_task(task: Task):
-    tasks.remove(task)
-    return task
+@app.delete("/delete/{id}")
+async def delete_task(id:UUID):
+    del tasks[id]
+    return "Success"
+
 
 #listagem de tarefas concluidas, não-concluidas, ou todas
 @app.get("/list/")
 async def list_task(q: Optional[bool] = None):
-    #if q is not None:
-        #if q:
-            #filtered_tasks = filtering(tasks,True)
-            #filtered_tasks =  [x for x in tasks if x["status"] == True]
-        #return n concluídas
-        #else:
-            #filtered_tasks = filtering(tasks,False)
-            #filtered_tasks =  [x for x in tasks if not x["status"] == False]
-        
-        #return filtered_tasks
-    
+    if q is not None:
+        if q:   
+            filtered_tasks = {k:v for k,v in tasks.items() if v["status"]}
+        else:
+            filtered_tasks =  {k:v for k,v in tasks.items() if not v["status"]}
+        return filtered_tasks
     
     return tasks
 
-    
-            
 
-        
-    return tasks
+
+
+
+
+#{k:v fpr k,v in tasks.items() if v.status}
